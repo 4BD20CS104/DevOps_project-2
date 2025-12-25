e14d092b4e3e0c3ecebc0d96a916ddfadba275d2
Syed <syedrihan223@gmail.com>
2025-12-25 23:42:05 +0530

    Adding one more ingress for jenkins port enabling.

--- Terraform/V4-EC2.tf
+++ Terraform/V4-EC2.tf
@@ -21,13 +21,20 @@ resource "aws_security_group" "demo-sg" {
     vpc_id = aws_vpc.dpp-aws.id
 
     ingress {
-        description = "ssh access"
+        description = "SSH access"
         from_port = 22
         to_port = 22
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
 
+    ingress {
+        description = "Jenkins port"
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
     egress {
         from_port = 0
         to_port = 0
